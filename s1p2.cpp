1e453713badba2fb4883cc16c8f115a8d5e9d07a
CL221043-PradeepGehlot <145000197+CL221043-PradeepGehlot@users.noreply.github.com>
2024-10-16 10:48:33 +0530

    Update s1p2.cpp

--- s1p2.cpp
+++ s1p2.cpp
@@ -4,13 +4,12 @@ using namespace std;
 int main() {
     int numbers[5];
     
-    // Taking 5 integers as input from the user
+
     cout << "Enter 5 integers: ";
     for (int i = 0; i < 5; i++) {
         cin >> numbers[i];
     }
     
-    // Finding the maximum number
     int max = numbers[0];
     for (int i = 1; i < 5; i++) {
         if (numbers[i] > max) {
@@ -18,7 +17,6 @@ int main() {
         }
     }
     
-    // Printing the maximum number
     cout << "The maximum number is: " << max << endl;
     
     return 0;
