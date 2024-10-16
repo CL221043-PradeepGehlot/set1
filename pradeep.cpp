b5188c0a3b428ddf8d8b19dc75eb122f6ac2c94d
CL221043-PradeepGehlot <145000197+CL221043-PradeepGehlot@users.noreply.github.com>
2024-10-16 10:23:35 +0530

    Update pradeep.cpp

--- pradeep.cpp
+++ pradeep.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Enter the number of elements in the array: ";
+    cout << "Enter the no. of elements in the array: ";
     cin >> n;
 
     int arr[n];
-    cout << "Enter the elements of the array: ";
+    cout << "Enter the elements of the array: "<<endl;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
